f1b6fe6917ea83636cac674bacc795d717271e08
Evgeniy Panchenko <panchenko.ten@gmail.com>
2021-08-21 22:03:40 +0300

    setup code of sliders

--- SliderRGB/ViewController.swift
+++ SliderRGB/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var ViewColorized: UIView!
+    
+    @IBOutlet var labelRedValue: UILabel!
+    @IBOutlet var LabelGreenValue: UILabel!
+    @IBOutlet var LabelBlueValue: UILabel!
+    
+    @IBOutlet var sliderRedValue: UISlider!
+    @IBOutlet var sliderGreenValue: UISlider!
+    @IBOutlet var sliderBlueValue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // ViewColorized
+        ViewColorized.layer.cornerRadius = 10
+        
+        // Label
+        labelRedValue.text = String(sliderRedValue.value)
+        LabelGreenValue.text = String(sliderGreenValue.value)
+        LabelBlueValue.text = String(sliderBlueValue.value)
     }
 
 
+    @IBAction func sliderActionRedValue() {
+        labelRedValue.text = String(sliderRedValue.value)
+    }
+    
+    @IBAction func sliderActionGreenValue() {
+        LabelGreenValue.text = String(sliderGreenValue.value)
+    }
+    
+    @IBAction func sliderActionBlueValue() {
+        LabelBlueValue.text = String(sliderBlueValue.value)
+    }
+    
+    
 }
 
